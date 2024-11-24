e0972fb8ca7021f799409c851c530f6f64ddaae3
storycraft <storycraft@pancake.sh>
2024-11-24 19:37:16 +0900

    feat: update type mangle table

--- src/compile/mangle.ts
+++ src/compile/mangle.ts
@@ -1,16 +1,18 @@
 import { FnSig } from '@/ast/fn';
 import { VarType } from '@/ast/types';
+import { IrType } from '@/ir/types';
 
-enum TypeMap {
-  number = 'N'
+const MANGLE_MAP: Record<IrType, string> = {
+  'number': 'N',
+  'empty': 'V',
 }
 
 export function mangle<
   Args extends VarType[],
   Ret extends VarType,
 >(sig: FnSig<Args, Ret>, id: number): string {
-  const args = sig.args.map(ty => TypeMap[ty]);
-  const ret = sig.returns ? TypeMap[sig.returns] : 'V';
+  const args = sig.args.map(ty => MANGLE_MAP[ty]);
+  const ret = MANGLE_MAP[sig.returns ?? 'empty'];
 
   return `_mcslib@${args}@${id}@${ret}`;
 }
