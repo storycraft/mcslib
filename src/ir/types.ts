0b3dfc0c89e391d27073ac681bab66c60cfd44fc
storycraft <storycraft@pancake.sh>
2024-11-25 13:17:03 +0900

    feat: remove size table as they are all 1

--- src/ir/types.ts
+++ src/ir/types.ts
@@ -21,19 +21,3 @@ export const IR_DEFAULT_CONST: Record<IrType, Const> = {
     value: null,
   },
 }
-
-/**
- * IR type information table
- */
-export const IR_TYPE_TABLE: Record<IrType, TyInfo> = {
-  'number': {
-    size: 1,
-  },
-  'empty': {
-    size: 0,
-  }
-}
-
-export type TyInfo = {
-  size: number,
-}
