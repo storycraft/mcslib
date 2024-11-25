f28da87f45e82b789b19de0a6cac341fffa5cc32
storycraft <storycraft@pancake.sh>
2024-11-25 12:53:08 +0900

    feat: split traversing

--- src/ir/node.ts
+++ src/ir/node.ts
@@ -19,7 +19,7 @@ export function emptyNode(
 }
 
 /**
- * Traverse each nodes from top
+ * Traverse node graph from the top
  * @param start node to start traverse
  * @param f function to run on each nodes
  */
@@ -32,27 +32,29 @@ export function traverseNode(
   for (let node = stack.pop(); node != null; node = stack.pop()) {
     f(node);
 
-    const end = node.end;
-    switch (end.ins) {
-      case 'jmp': {
-        stack.push(end.next);
-        break;
-      }
+    stack.push(...childrenNodes(node));
+  }
+}
 
-      case 'switch_int': {
-        f(node);
-        for (const node of end.table) {
-          if (node) {
-            stack.push(node);
-          }
-        }
-        stack.push(end.default);
-        break;
-      }
+/**
+ * Get next nodes
+ * @param node parent node
+ */
+export function childrenNodes(
+  node: Node,
+): Node[] {
+  const end = node.end;
+  switch (end.ins) {
+    case 'jmp': {
+      return [end.next];
+    }
 
-      default: {
-        break;
-      }
+    case 'switch_int': {
+      return [...end.table.filter(node => node != null), end.default];
+    }
+
+    default: {
+      return [];
     }
   }
 }
