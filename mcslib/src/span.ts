1f0d99743bf9df6fff98f9ac35e4d6aeeba40694
storycraft <storycraft@pancake.sh>
2024-12-02 13:04:11 +0900

    feat: implement best effort span

--- mcslib/src/span.ts
+++ mcslib/src/span.ts
@@ -1,13 +1,33 @@
-export type Span = {
+import StackTracey from 'stacktracey';
 
+/**
+ * Represent the approximate location of the source
+ */
+export type Span = {
+  /**
+   * Location of the span
+   */
+  location: string,
+  /**
+   * Line number in the file
+   */
+  line?: number,
+
+  /**
+   * Column number in the file
+   */
+  col?: number,
 }
 
 /**
  * Create a new span of the call site
  */
 export function callSite(): Span {
+  const pos = new StackTracey().items[1];
   return {
-
+    location: pos.fileRelative,
+    line: pos.line,
+    col: pos.column,
   };
 }
 
@@ -16,6 +36,6 @@ export function callSite(): Span {
  */
 export function unknownSpan(): Span {
   return {
-
+    location: '<unknown>',
   };
 }
