08214303e91d4f57c70638961067a5251263f970
storycraft <storycraft@pancake.sh>
2024-12-02 14:06:04 +0900

    feat: update example

--- examples/build.ts
+++ examples/build.ts
@@ -7,19 +7,25 @@ const path = 'example_pack.zip';
 const writer = new DatapackWriter('example', createWriteStream(path));
 
 await writer.addStream('pack.mcmeta', createReadStream('./examples/pack.mcmeta'));
-await writer.export({
-  name: 'draw_star',
-  fn: draw_star,
-  args: ['size'],
-});
-console.log(`exported draw_star function`);
+if (
+  await writer.export({
+    name: 'draw_star',
+    fn: draw_star,
+    args: ['size'],
+  })
+) {
+  console.log(`exported draw_star function`);
+}
 
-await writer.export({
-  name: 'cube_particle',
-  fn: cubeParticle,
-  args: ['width', 'length', 'height', 'density'],
-});
-console.log(`exported cube_particle function`);
+if (
+  await writer.export({
+    name: 'cube_particle',
+    fn: cubeParticle,
+    args: ['width', 'length', 'height', 'density'],
+  })
+) {
+  console.log(`exported cube_particle function`);
+}
 
 await writer.finish();
 
